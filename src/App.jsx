b209c42952edb9a276ee8d94725f9e82579d6e60
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-19 08:55:18 -0700

    Update import statement for HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage';
+import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 
