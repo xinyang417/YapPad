6bd2dad115b7fe9f92e36e253915b938ff549f2c
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-28 10:04:58 -0700

    Customized console message to only show appropriate messages

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,7 +1,9 @@
+// Backend Imports
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+// Frontend Imports
 import '../style/SignupPage.css';
 import '../App.css';
 import logo from '../assets/images/logo.png';
@@ -13,20 +15,16 @@ function SignupPage() {
 
     const handleSignup = async (event) => {
         event.preventDefault();
-        console.log("Signup attempt with:", username, password, email);
+        console.log("Signup attempt with", email);
 
         try {
-            // Make a POST request to the backend signup endpoint
             const response = await axios.post('http://localhost:8000/auth/register', {
                 username: username,
                 email: email,
                 password: password
             });
-
-            // Handle successful signup
-            console.log('Signup successful:', response.data);
+            console.log('Signup successful.', response.data.message);
         } catch (error) {
-            // Handle signup error
             console.error('Signup failed:', error.message);
         }
     };
@@ -38,6 +36,16 @@ function SignupPage() {
                     <img src={logo} alt="Logo" className="signup-logo" />
                 </div>
                 <h2>Sign Up</h2>
+                <div className="input-group">
+                    <label htmlFor="email">Email (will be used for Login)</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
                 <div className="input-group">
                     <label htmlFor="username">Username</label>
                     <input
@@ -56,16 +64,6 @@ function SignupPage() {
                         name="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
                 <button type="submit" className="signup-button">Sign Up</button>
